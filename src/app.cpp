c0971cdfeb27c91c1d727feac3b010d21fdff94f
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-03-24 14:40:58 +0100

    added Camera class, view and projection transformation matrices

--- src/app.cpp
+++ src/app.cpp
@@ -3,6 +3,8 @@
 //=============================================================================================
 #include "../inc/framework.h"
 
+using namespace::glm;
+
 // cs�cspont �rnyal�
 const char * vertSource = R"(
 	#version 330				
@@ -28,13 +30,86 @@ const char * fragSource = R"(
 	}
 )";
 
+class Camera {
+public:
+	/**
+	 * Kamera konstruktor.
+	 * 
+	 * @param wCenter A kamera középpontja világ koordináta szerint.
+	 * @param wWidth A kamera szélessége világ koordináta szerint.
+	 * @param wHeight A kamera magassága világ koordináta szerint.
+	 */
+	Camera(const vec3& wCenter, float wWidth, float wHeight) {
+		this->wCenter = wCenter;
+		this->wWidth = wWidth;
+		this->wHeight = wHeight;
+	}
+
+	/**
+	 * Kiszámítja a model transzformációs mátrixot a kamera tulajdonságaiból.
+	 */
+	mat4 model() {
+
+	}
+
+	/**
+	 * Kiszámítja a model transzformációs mátrix inverzét a kamera tulajdonságaiból.
+	 */
+	mat4 invModel() {
+
+	}
+	
+	/**
+	 * Kiszámítja a view transzformációs mátrixot a kamera tulajdonságaiból.
+	 */
+	mat4 view() {
+		return mat4 {
+			vec4(1.0f, 0.0f, 0.0f, -wCenter.x),
+			vec4(0.0f, 1.0f, 0.0f, -wCenter.y),
+			vec4(0.0f, 0.0f, 1.0f, 0.0f),
+			vec4(0.0f, 0.0f, 0.0f, 1.0f)
+		};
+	}
+
+	/**
+	 * Kiszámítja a view transzformációs mátrix inverzét a kamera tulajdonságaiból.
+	 */
+	mat4 invView() {
+
+	}
+
+	/**
+	 * Kiszámítja a projection transzformációs mátrixot a kamera tulajdonságaiból.
+	 */
+	mat4 projection() {
+		return mat4 {
+			vec4((2.0f / wWidth),	0.0f, 				0.0f, 0.0f),
+			vec4(0.0f,				(2.0f / wHeight),	0.0f, 0.0f),
+			vec4(0.0f, 				0.0f, 				1.0f, 0.0f),
+			vec4(0.0f, 				0.0f, 				0.0f, 1.0f)
+		};
+	}
+
+	/**
+	 * Kiszámítja a projection transzformációs mátrix inverzét a kamera tulajdonságaiból.
+	 */
+	mat4 invProjection() {
+
+	}
+
+private:	
+	vec3 wCenter;
+	float wWidth;
+	float wHeight;
+};
+
 const int winWidth = 600, winHeight = 600;
 
 class GreenTriangleApp : public glApp {
 	Geometry<vec3>* triangle;  // geometria
 	GPUProgram* gpuProgram;	   // cs�cspont �s pixel �rnyal�k
 public:
-	GreenTriangleApp() : glApp("Green triangle") { }
+	GreenTriangleApp() : glApp("Lab2") { }
 
 	// Inicializ�ci�, 
 	void onInitialization() {
